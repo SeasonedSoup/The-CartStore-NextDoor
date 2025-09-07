711a9cc91976d0d1671cb9881703d7f65131ee00
SeasonedSoup <emmetteagoncillo1@gmail.com>
2025-09-07 09:37:10 +0800

    Add Skeleton And Card Example for ShopPage

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+//a card for each item in the store skeleton first
+export function Card() {
+    return (
+        <div>
+            <div>
+                <img src="#" alt="CoolStuff" />
+                <h3>Product Name</h3>
+                <h3>Product Description</h3>
+                <div className="price&amount">
+                    <h2>Price</h2>
+                    <h2>Product Amount</h2>
+                </div>
+                <button className="addToCart">Add To Cart</button>
+            </div>
+        </div>
+    )
+}
--- src/components/ShopPage.jsx
+++ src/components/ShopPage.jsx
@@ -1,10 +1,14 @@
 import { Nav } from "./Nav"
-
+import { Card } from "./Card"
 export function ShopPage() {
     return (
         <>
             <Nav />
             <h1>Hello Shop Page is Currently a WORK IN PROGRESS!</h1>
+            <h2> This is where to browse items and add them to your cart!</h2>
+            <label htmlFor="search">Search: </label>
+            <input type="search" id="search" name="search" />
+            <Card></Card>
         </>
     )
 }
