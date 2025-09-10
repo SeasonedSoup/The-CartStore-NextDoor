f560314e658c53df7b0c3ee15e7b95e8cb27a208
SeasonedSoup <emmetteagoncillo1@gmail.com>
2025-09-11 05:50:52 +0800

    Add Use Effect and state to fetch items on cart
    mainly on app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,27 @@ import { Navbar } from "./components/Navbar"
 import { Outlet } from "react-router"
 
 import "./App.css"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 function App() {
 
-  const [cart, setCart] = useState(3)
+  const [cart, setCart] = useState([])
+  
+  useEffect(() => {
+    async function fetchItems() {
+      const response = await fetch('https://fakestoreapi.com/products');
+      const items = await response.json();
+      
+      setCart(items)
+    } 
+    fetchItems()
+  }, [])
+
+  console.log(cart)
 
   return (
     <>
-      <Navbar cart={cart}/>
-      <Outlet />
+      <Navbar />
+      <Outlet context={[cart, setCart]}/>
     </>
   )
 }
