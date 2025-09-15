c882f6bb6b57b59fae0dcf02f25cf35a2a0d464a
SeasonedSoup <emmetteagoncillo1@gmail.com>
2025-09-16 07:36:10 +0800

    Add new state products
    cart will instead be the users items that have
    been added to cart

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import { useState, useEffect } from "react"
 function App() {
 
   const [cart, setCart] = useState([])
-  
+  const [products, setProducts] = useState([])
   useEffect(() => {
     async function fetchItems() {
       const response = await fetch('https://fakestoreapi.com/products');
       const items = await response.json();
       
-      setCart(items)
+      setProducts(items)
     } 
     fetchItems()
   }, [])
