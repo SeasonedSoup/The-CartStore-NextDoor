ccabc6f63f6f551b30a192412f352fa48574d617
SeasonedSoup <emmetteagoncillo1@gmail.com>
2025-09-18 17:30:01 +0800

    Add Amount Indicator on Navbar
    still on prototype

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,16 @@ import "../styles/styles.css"
 import { NavLink } from "react-router-dom";
 
 export function Navbar({cart}) {
+
+    const cartAmount = cart.length
+    
     return (
         <nav>
             <h1 className="logo">The-Cart-Store-NextDoor</h1>
             <NavLink to="/"> Home</NavLink>
             <NavLink to="/shoppage">Shop</NavLink>
             <NavLink to="/cartpage">Cart 
-                <span className="indicator">{cart}</span>
+                <span className="indicator">{cartAmount}</span>
             </NavLink>
         </nav>
     )
