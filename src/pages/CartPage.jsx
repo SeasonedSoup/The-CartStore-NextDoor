127cb327399e8daeee02890d16bcec12dbda243b
SeasonedSoup <emmetteagoncillo1@gmail.com>
2025-09-18 20:58:10 +0800

    Spruced up HomePage with image and centered text

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -4,6 +4,16 @@
  export function CartPage() {
 
     const {cart, setCart} = useOutletContext();
+
+    function buyEverything() {
+        if (cart.length === 0) {
+            alert("Your cart is empty!")
+            return
+        }
+        
+        alert("Thanks for buying everything!")
+        setCart([])
+    }
     console.log(cart)
     return (
         <>
@@ -11,12 +21,12 @@
             <div className="items">
                 <h1> Item: Quantity Increase and Decrease Quantity Price</h1>
             </div>
-            <div className="checkout">
-                <button>Check Out!</button>
-            </div>
             {cart.map(item => (
                 <h1 key={item.id}>{item.title}{item.quantity}</h1>
             ))}
+            <div className="checkout">
+                <button onClick={buyEverything}>Check Out!</button>
+            </div>
         </>
     )
  }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,10 +3,10 @@ import cartStoreImg from "../assets/cartStore.jpg"
 
 export function HomePage() {
     return (
-        <>
-            <h1>The CartStore-NextDoor <img src={cart} style={{width: "36px"}} alt="cart" /></h1> 
-            <h2>Welcome To The Cart Page Next Door!</h2>
-            <h3>Where you can buy stuff!</h3> 
-        </>
+        <div className="homePage">
+            <h2>Welcome To The Cart Page Next Door!  <img src={cart} style={{width: "36px", display: "inline"}} alt="cart" /></h2>
+            <h2>Where you can buy stuff!</h2> 
+            <img className="homeImg" src={cartStoreImg} alt="image of our location!" />
+        </div>
     )
 }
